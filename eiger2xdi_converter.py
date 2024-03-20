e00fd5761eb887a9aee5671a228034181d054fbc
alexisforestier <alforestier@gmail.com>
2024-03-20 08:15:03 +0100

    Progress bar min/max fixed, now using QLineEdit for source and dest

--- eiger2xdi_converter.py
+++ eiger2xdi_converter.py
@@ -4,6 +4,7 @@ import fabio
 from PyQt5.QtWidgets import (QApplication, 
                              QWidget,
                              QLabel,
+                             QLineEdit,
                              QPushButton,
                              QListWidget,
                              QFormLayout,
@@ -17,7 +18,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.resize(500,150)
+        self.resize(400,150)
 
         self.sourcepath = str()
         self.destpath = str()
@@ -33,18 +34,18 @@ class MainWindow(QWidget):
         toplayout.addWidget(self.button_dest)
 
 
-        self.label_source = QLabel('')
-        self.label_dest = QLabel('')
+        self.lineedit_source = QLineEdit('')
+        self.lineedit_dest = QLineEdit('')
 
-        self.label_source.setStyleSheet("border: 1px solid black;\
+        self.lineedit_source.setStyleSheet("border: 1px solid black;\
                                    background-color:#d8fff7")
-        self.label_dest.setStyleSheet("border: 1px solid black;\
+        self.lineedit_dest.setStyleSheet("border: 1px solid black;\
                                    background-color:#d8fff7")
 
 
         form = QFormLayout()
-        form.addRow('Source HDF5 file:   ', self.label_source)
-        form.addRow('Destination:        ', self.label_dest)
+        form.addRow('Source HDF5 file:   ', self.lineedit_source)
+        form.addRow('Destination:        ', self.lineedit_dest)
 
         self.button_go = QPushButton('Go !')
         self.button_clear = QPushButton('Clear all')
@@ -53,12 +54,12 @@ class MainWindow(QWidget):
         bottom_layout.addWidget(self.button_go)
         bottom_layout.addWidget(self.button_clear)
 
-        self.progress = QProgressBar()
+        self.progress_bar = QProgressBar()
 
         layout.addLayout(toplayout)
         layout.addLayout(form)
         layout.addLayout(bottom_layout)
-        layout.addWidget(self.progress)
+        layout.addWidget(self.progress_bar)
 
         self.setLayout(layout)
 
@@ -66,6 +67,17 @@ class MainWindow(QWidget):
         self.button_dest.clicked.connect(self.choose_dest)
         self.button_go.clicked.connect(self.go)
         self.button_clear.clicked.connect(self.clear_all)
+
+        self.lineedit_source.textChanged.connect(self.update_source)
+        self.lineedit_dest.textChanged.connect(self.update_dest)
+
+
+    def update_source(self, s):
+        self.sourcepath = s
+
+
+    def update_dest(self, s):
+        self.destpath = s
 
 
     def choose_source(self):
@@ -77,7 +89,7 @@ class MainWindow(QWidget):
             "Choose HDF5 file", "","HDF5 file (*.h5)", 
             options=options)        
         
-        self.label_source.setText(self.sourcepath)
+        self.lineedit_source.setText(self.sourcepath)
 
 
     def choose_dest(self):
@@ -89,34 +101,42 @@ class MainWindow(QWidget):
         self.destpath = QFileDialog.getExistingDirectory(self,
                  'Select Destination', options=options)
 
-        self.label_dest.setText(self.destpath)
+        self.lineedit_dest.setText(self.destpath)
 
 
     def go(self):
         if self.sourcepath and self.destpath:
             try:
                 img = fabio.open(self.sourcepath)
-    
-                for i in range(img.nframes):
+                nframes = img.nframes
+
+                self.progress_bar.setMinimum(0)
+                self.progress_bar.setMaximum(nframes)
+
+                for i in range(nframes):
                     frame = img.getframe(i)
                     conv = frame.convert('tifimage')
                     data = conv.data.astype('int32')
                     data[data < 0] = 0
                     conv.data = data
     
-                    conv.write(self.destpath + '/frame_'+ '%04d' % (i+1) + '.tif')
-                    self.progress.setValue(i+1)
+                    conv.write(self.destpath + 
+                                '/frame_'+ '%04d' % (i+1) + '.tif')
+                    
+                    self.progress_bar.setValue(i+1)
 
             except Exception as e:
+
                 QMessageBox.critical(self, 'Error', str(e))
+                self.progress_bar.reset()
 
 
     def clear_all(self):
         self.sourcepath = str()
         self.destpath = str()
-        self.label_source.clear()
-        self.label_dest.clear()
-        self.progress.setValue(0)
+        self.lineedit_source.clear()
+        self.lineedit_dest.clear()
+        self.progress_bar.reset()
 
 
 if __name__ == '__main__':
